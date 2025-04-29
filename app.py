f51d504bb4622f88ebde422423f3d3255caef51a
Harjot Singh <harjot8870@gmail.com>
2025-04-29 19:50:09 +0530

    Added weather app with Flask, styled with CSS

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = '41c6afd86b5ef7fcdb78cba22201eea4'  # Replace with your OpenWeatherMap API key
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form.get('city')
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather_data = {
+                'city': city,
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'].title(),
+                'icon': data['weather'][0]['icon']
+            }
+        else:
+            weather_data = {'error': 'City not found'}
+
+    return render_template('index.html', weather=weather_data)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
